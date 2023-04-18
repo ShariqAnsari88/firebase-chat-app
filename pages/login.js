8c557dcff66d66d1775d477bfe35463b7ff384ab
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-04-19 02:46:23 +0530

    auth completed

--- pages/login.js
+++ pages/login.js
@@ -4,6 +4,9 @@ import { useRouter } from "next/router";
 import { useAuth } from "@/firebase/authContext";
 import Loader from "@/components/Loader";
 
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebase/firebase";
+
 const Login = () => {
     const router = useRouter();
     const { currentUser, isLoading } = useAuth();
@@ -14,13 +17,36 @@ const Login = () => {
         }
     }, [currentUser, isLoading]);
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const email = e.target[0].value;
+        const password = e.target[1].value;
+
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return isLoading || (!isLoading && !!currentUser) ? (
         <Loader />
     ) : (
         <div className="h-[100vh] flex justify-center items-center">
-            <form className="flex flex-col gap-3 w-[350px] p-5 bg-gray-600">
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
+            <form
+                className="flex flex-col gap-3 w-[350px] p-5 bg-gray-600"
+                onSubmit={handleSubmit}
+            >
+                <input
+                    type="email"
+                    placeholder="Email"
+                    className="text-black"
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="text-black"
+                />
                 <button>Login</button>
             </form>
         </div>
