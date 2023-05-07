ca9621990d599cbbc98138421c3ac3ea12872f14
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-05-07 18:31:42 +0530

    update profile actions completed

--- components/LeftNav.jsx
+++ components/LeftNav.jsx
@@ -7,46 +7,125 @@ import { IoLogOutOutline, IoClose } from "react-icons/io5";
 import { FiPlus } from "react-icons/fi";
 import { BiCheck, BiEdit } from "react-icons/bi";
 import { MdAddAPhoto, MdPhotoCamera, MdDeleteForever } from "react-icons/md";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+
 import UsersPopup from "./popup/UsersPopup";
 import { profileColors } from "@/utils/constants";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
-import { db } from "@/firebase/firebase";
+import { db, auth, storage } from "@/firebase/firebase";
 import { useChatContext } from "@/context/chatContext";
+import ToastMessage from "./ToastMessage";
+import { toast } from "react-toastify";
+import { updateProfile } from "firebase/auth";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 
 const LeftNav = () => {
     const [userPopup, setUserPopup] = useState(false);
     const [editProfile, setEditProfile] = useState(false);
+    const [nameEdited, setNameEdited] = useState(false);
+
     const { signOut, currentUser, setCurrentUser } = useAuth();
     const { data } = useChatContext();
+
+    const authUser = auth.currentUser;
 
     const handleUpdateProfile = async (type, value) => {
         let obj = { ...currentUser };
         switch (type) {
             case "color":
                 obj.color = value;
-
                 break;
-
+            case "name":
+                obj.displayName = value;
+                break;
+            case "photo":
+                obj.photoURL = value;
+                break;
+            case "photo-remove":
+                obj.photoURL = null;
+                break;
             default:
                 break;
         }
 
-        const userDocRef = doc(db, "users", currentUser.uid);
-        await updateDoc(userDocRef, obj);
+        try {
+            toast.promise(
+                async () => {
+                    const userDocRef = doc(db, "users", currentUser.uid);
+                    await updateDoc(userDocRef, obj);
+                    setCurrentUser(obj);
 
-        // const userChatDocRef = doc(db, "userChats", currentUser.uid);
-        // const userChatDoc = await getDoc(userChatDocRef);
-        // let updatedUserChatDoc = { ...userChatDoc.data() };
-        // updatedUserChatDoc[currentUser.uid + currentUser.uid].userInfo.color =
-        //     value;
-        // await updateDoc(userChatDocRef, updatedUserChatDoc);
+                    if ("photo-remove") {
+                        await updateProfile(authUser, {
+                            photoURL: null,
+                        });
+                    }
 
-        setCurrentUser(obj);
+                    if (type === "name") {
+                        await updateProfile(authUser, {
+                            displayName: value,
+                        });
+                        setNameEdited(false);
+                    }
+                },
+                {
+                    pending: "Updating profile.",
+                    success: "Profile updated successfully.",
+                    error: "Profile udpate failed.",
+                },
+                {
+                    autoClose: 3000,
+                }
+            );
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const uploadImageToFirebase = async (file) => {
+        try {
+            if (file) {
+                const storageRef = ref(storage, currentUser.displayName);
+                const uploadTask = uploadBytesResumable(storageRef, file);
+                uploadTask.on(
+                    "state_changed",
+                    (snapshot) => {},
+                    (error) => {
+                        console.error(error);
+                    },
+                    () => {
+                        getDownloadURL(uploadTask.snapshot.ref).then(
+                            async (downloadURL) => {
+                                handleUpdateProfile("photo", downloadURL);
+                                await updateProfile(authUser, {
+                                    photoURL: downloadURL,
+                                });
+                            }
+                        );
+                    }
+                );
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const onkeyup = (event) => {
+        if (event.target.innerText.trim() !== currentUser.displayName) {
+            setNameEdited(true);
+        } else {
+            setNameEdited(false);
+        }
+    };
+    const onkeyDown = (event) => {
+        if (event.key === "Enter" && event.keyCode === 13)
+            event.preventDefault();
     };
 
     const editProfileContainer = () => {
         return (
             <div className="flex flex-col items-center relative">
+                <ToastMessage />
                 <Icon
                     size="small"
                     className="absolute top-0 right-5"
@@ -60,27 +139,56 @@ const LeftNav = () => {
                         onClick={() => setEditProfile(true)}
                     />
                     <div className="w-full h-full rounded-full bg-black/[0.5] absolute top-0 left-0 justify-center items-center hidden group-hover:flex">
-                        {currentUser.photoURL ? (
-                            <MdPhotoCamera size={34} />
-                        ) : (
-                            <MdAddAPhoto size={34} />
-                        )}
+                        <label htmlFor="fileUpload">
+                            {currentUser.photoURL ? (
+                                <MdPhotoCamera size={34} />
+                            ) : (
+                                <MdAddAPhoto size={34} />
+                            )}
+                        </label>
+                        <input
+                            id="fileUpload"
+                            type="file"
+                            onChange={(e) =>
+                                uploadImageToFirebase(e.target.files[0])
+                            }
+                            style={{ display: "none" }}
+                        />
                     </div>
 
                     {currentUser.photoURL && (
-                        <div className="w-8 h-8 rounded-full bg-black flex justify-center items-center absolute right-0 bottom-0">
-                            <MdDeleteForever />
+                        <div
+                            className="w-6 h-6 rounded-full bg-red-500 flex justify-center items-center absolute right-0 bottom-0"
+                            onClick={() => handleUpdateProfile("photo-remove")}
+                        >
+                            <MdDeleteForever size={14} />
                         </div>
                     )}
                 </div>
                 <div className="mt-5 flex flex-col items-center">
                     <div className="flex items-center gap-2">
-                        <BiEdit color="#8B8D93" />
+                        {!nameEdited && <BiEdit color="#8B8D93" />}
+                        {nameEdited && (
+                            <BsFillCheckCircleFill
+                                className="cursor-pointer"
+                                color="#6b8afd"
+                                onClick={() =>
+                                    handleUpdateProfile(
+                                        "name",
+                                        document.getElementById(
+                                            "displayNameEdit"
+                                        ).innerText
+                                    )
+                                }
+                            />
+                        )}
                         <div
                             contentEditable
                             className="bg-transparent outline-none border-none text-center"
                             type="text"
                             id="displayNameEdit"
+                            onKeyUp={onkeyup}
+                            onKeyDown={onkeyDown}
                         >
                             {currentUser.displayName}
                         </div>
