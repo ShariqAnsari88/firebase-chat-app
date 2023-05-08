e999d437df5019ea121519e1ea400f9d2656792d
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-05-09 03:08:48 +0530

    login and register page UI completed

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 import { useAuth } from "@/context/authContext";
 import Loader from "@/components/Loader";
 
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    FacebookAuthProvider,
+    signInWithPopup,
+    sendPasswordResetEmail,
+} from "firebase/auth";
 import { auth } from "@/firebase/firebase";
+const gProvider = new GoogleAuthProvider();
+const fProvider = new FacebookAuthProvider();
+
+import { IoLogoGoogle, IoLogoFacebook } from "react-icons/io";
+import ToastMessage from "@/components/ToastMessage";
+import { toast } from "react-toastify";
+import Link from "next/link";
 
 const Login = () => {
     const router = useRouter();
     const { currentUser, isLoading } = useAuth();
+    const [email, setEmail] = useState("");
 
     useEffect(() => {
         if (!isLoading && currentUser) {
@@ -29,26 +43,121 @@ const Login = () => {
         }
     };
 
+    const resetPassword = async () => {
+        try {
+            toast.promise(
+                async () => {
+                    await sendPasswordResetEmail(auth, email);
+                },
+                {
+                    pending: "Generating reset link",
+                    success: "Reset email send to your registered email id.",
+                    error: "You may have entered wrong email id!",
+                },
+                {
+                    autoClose: 5000,
+                }
+            );
+            console.log("Email send to your registered email id.");
+        } catch (error) {
+            console.error("An error occured", error);
+        }
+    };
+
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, gProvider);
+        } catch (error) {
+            console.error("An error occured", error);
+        }
+    };
+
+    const signInWithFacebook = async () => {
+        try {
+            await signInWithPopup(auth, fProvider);
+        } catch (error) {
+            console.error("An error occured", error);
+        }
+    };
+
     return isLoading || (!isLoading && !!currentUser) ? (
         <Loader />
     ) : (
-        <div className="h-[100vh] flex justify-center items-center">
-            <form
-                className="flex flex-col gap-3 w-[350px] p-5 bg-gray-600"
-                onSubmit={handleSubmit}
-            >
-                <input
-                    type="email"
-                    placeholder="Email"
-                    className="text-black"
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    className="text-black"
-                />
-                <button>Login</button>
-            </form>
+        <div className="h-[100vh] flex justify-center items-center bg-[#131313]">
+            <ToastMessage />
+            <div className="flex items-center flex-col">
+                <div className="text-center">
+                    <div className="text-4xl font-bold">
+                        Login to Your Account
+                    </div>
+                    <div className="mt-3 text-[#8B8D93]">
+                        Connect and chat with anyone, anywhere
+                    </div>
+                </div>
+                <div className="flex items-center gap-2 w-full mt-10 mb-5">
+                    <div
+                        className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-1/2 h-14 rounded-md cursor-pointer p-[1px]"
+                        onClick={signInWithGoogle}
+                    >
+                        <div className="flex items-center justify-center gap-3 text-white font-semibold bg-[#131313] w-full h-full rounded-md">
+                            <IoLogoGoogle size={24} />
+                            <span>Login with Google</span>
+                        </div>
+                    </div>
+                    <div
+                        className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-1/2 h-14 rounded-md cursor-pointer p-[1px]"
+                        onClick={signInWithFacebook}
+                    >
+                        <div className="flex items-center justify-center gap-3 text-white font-semibold bg-[#131313] w-full h-full rounded-md">
+                            <IoLogoFacebook size={24} />
+                            <span>Login with Facebook</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center gap-1">
+                    <span className="w-5 h-[1px] bg-[#8B8D93]"></span>
+                    <span className="text-[#8B8D93] font-semibold">OR</span>
+                    <span className="w-5 h-[1px] bg-[#8B8D93]"></span>
+                </div>
+                <form
+                    className="flex flex-col items-center gap-3 w-[500px] mt-5"
+                    onSubmit={handleSubmit}
+                >
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="w-full h-14 bg-[#2E343D] rounded-xl outline-none border-none px-5 text-[#8B8D93]"
+                        autocomplete="off"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="w-full h-14 bg-[#2E343D] rounded-xl outline-none border-none px-5 text-[#8B8D93]"
+                        autocomplete="off"
+                    />
+                    <div className="text-right w-full text-[#8B8D93]">
+                        <span
+                            className="cursor-pointer"
+                            onClick={resetPassword}
+                        >
+                            Forgot Password?
+                        </span>
+                    </div>
+                    <button className="mt-4 w-full h-14 rounded-xl outline-none text-base font-semibold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+                        Login to Your Account
+                    </button>
+                </form>
+                <div className="flex justify-center gap-1 text-[#8B8D93] mt-5">
+                    <span>Not a member yet?</span>
+                    <Link
+                        href="/register"
+                        className="font-semibold text-white underline underline-offset-2 cursor-pointer"
+                    >
+                        Register Now
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
--- pages/register.js
+++ pages/register.js
@@ -2,13 +2,25 @@ import { useEffect } from "react";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { auth, storage } from "../firebase/firebase";
 import { db } from "../firebase/firebase";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {
+    createUserWithEmailAndPassword,
+    updateProfile,
+    GoogleAuthProvider,
+    FacebookAuthProvider,
+    signInWithPopup,
+} from "firebase/auth";
+const gProvider = new GoogleAuthProvider();
+const fProvider = new FacebookAuthProvider();
+
 import { doc, setDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
 
 import { useAuth } from "@/context/authContext";
 import Loader from "@/components/Loader";
 import { profileColors } from "@/utils/constants";
+
+import { IoLogoGoogle, IoLogoFacebook } from "react-icons/io";
+import Link from "next/link";
 
 const Register = () => {
     const router = useRouter();
@@ -105,35 +117,98 @@ const Register = () => {
         }
     };
 
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, gProvider);
+        } catch (error) {
+            console.error("An error occured", error);
+        }
+    };
+
+    const signInWithFacebook = async () => {
+        try {
+            await signInWithPopup(auth, fProvider);
+        } catch (error) {
+            console.error("An error occured", error);
+        }
+    };
+
     return isLoading || (!isLoading && !!currentUser) ? (
         <Loader />
     ) : (
-        <div className="h-[100vh] flex justify-center items-center">
-            <form
-                onSubmit={handleSubmit}
-                className="flex flex-col gap-3 w-[350px] p-5 bg-gray-600"
-            >
-                <input
-                    type="text"
-                    placeholder="Display Name"
-                    className="text-black"
-                    autoComplete="off"
-                />
-                <input
-                    type="email"
-                    placeholder="Email"
-                    className="text-black"
-                    autoComplete="off"
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    className="text-black"
-                    autoComplete="off"
-                />
-                <input type="file" />
-                <button>Sign Up</button>
-            </form>
+        <div className="h-[100vh] flex justify-center items-center bg-[#131313]">
+            <div className="flex items-center flex-col">
+                <div className="text-center">
+                    <div className="text-4xl font-bold">Create New Account</div>
+                    <div className="mt-3 text-[#8B8D93]">
+                        Connect and chat with anyone, anywhere
+                    </div>
+                </div>
+                <div className="flex items-center gap-2 w-full mt-10 mb-5">
+                    <div
+                        className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-1/2 h-14 rounded-md cursor-pointer p-[1px]"
+                        onClick={signInWithGoogle}
+                    >
+                        <div className="flex items-center justify-center gap-3 text-white font-semibold bg-[#131313] w-full h-full rounded-md">
+                            <IoLogoGoogle size={24} />
+                            <span>Login with Google</span>
+                        </div>
+                    </div>
+                    <div
+                        className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-1/2 h-14 rounded-md cursor-pointer p-[1px]"
+                        onClick={signInWithFacebook}
+                    >
+                        <div className="flex items-center justify-center gap-3 text-white font-semibold bg-[#131313] w-full h-full rounded-md">
+                            <IoLogoFacebook size={24} />
+                            <span>Login with Facebook</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center gap-1">
+                    <span className="w-5 h-[1px] bg-[#8B8D93]"></span>
+                    <span className="text-[#8B8D93] font-semibold">OR</span>
+                    <span className="w-5 h-[1px] bg-[#8B8D93]"></span>
+                </div>
+                <form
+                    onSubmit={handleSubmit}
+                    className="flex flex-col items-center gap-3 w-[500px] mt-5"
+                >
+                    <input
+                        type="text"
+                        placeholder="Display Name"
+                        className="w-full h-14 bg-[#2E343D] rounded-xl outline-none border-none px-5 text-[#8B8D93]"
+                        autoComplete="off"
+                        required
+                    />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="w-full h-14 bg-[#2E343D] rounded-xl outline-none border-none px-5 text-[#8B8D93]"
+                        autoComplete="off"
+                        required
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="w-full h-14 bg-[#2E343D] rounded-xl outline-none border-none px-5 text-[#8B8D93]"
+                        autoComplete="off"
+                        required
+                    />
+                    {/* <input type="file" /> */}
+                    <button className="mt-4 w-full h-14 rounded-xl outline-none text-base font-semibold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+                        Sign Up
+                    </button>
+                </form>
+                <div className="flex justify-center gap-1 text-[#8B8D93] mt-5">
+                    <span>Already have an account?</span>
+                    <Link
+                        href="/login"
+                        className="font-semibold text-white underline underline-offset-2 cursor-pointer"
+                    >
+                        Login
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
